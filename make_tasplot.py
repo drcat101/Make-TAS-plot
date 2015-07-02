d26d618c8d9aaf8c9dfc3363b19af5aecbe683e3
drcat101 <drcat101@users.noreply.github.com>
2015-07-01 20:35:45 -0700

    Update make_tasplot.py

--- make_tasplot.py
+++ make_tasplot.py
@@ -6,38 +6,32 @@
 
 def makeTAS(csvFile, dataLabel, outputFileName):
 
-    #import required modules
+    # Import the required modules
     import numpy as np
     import pandas as pd
     import matplotlib.pyplot as plt
     import tasplot
 
-    # Read in .csv file
+    # Read in .csv file to Pandas dataframe
     df = pd.read_csv(csvFile, index_col=0)
     silica = df['SiO2'].tolist()
     alk = np.array(df['Na2O']) + np.array(df['K2O'])
     alkalis = alk.tolist()
 
-    #change font
+    # Change the font for all text and labels on the final plot
     plt.rcParams['font.family'] = 'Arial'
 
     # Set up figure
     ax1 = plt.subplot(111)  # create axes and store as variable
     tasplot.add_LeMaitre_fields(ax1)  # add TAS fields to plot
 
-    # Note that you can change the default colour and font size e.g.
-    # >>> tasplot.add_LeMaitre_Fields(ax1, color='red', fontsize=8)
-
-    #Plot the data
+    # Plot the data
     ax1.plot(silica, alkalis, 'd', markerfacecolor='k', markeredgecolor='k', markersize=8, label=dataLabel)
     
     # Decorate the plot
     plt.xlabel(r'SiO$_2$ (wt%)')  # Use LaTeX notation for subscript
     plt.ylabel(r'Na$_2$O + K$_2$O (wt%)')
-    #plt.legend(loc='upper right', numpoints=1)
     name = outputFileName + '.pdf'
-    plt.savefig(name, dpi=300, bbox_inches='tight', pad_inches=0.25)
+    plt.savefig(name, dpi = 300, bbox_inches = 'tight', pad_inches = 0.25)
 
-
-makeTAS("XRF.csv", 'Delta', 'Figure 10')
 
